5192264ba823fbcca385685e49e9d26eddacc84a
Osman <seitmemetov228@gmail.com>
2023-12-24 20:44:04 +0300

    changed connection

--- db.js
+++ db.js
@@ -2,9 +2,10 @@ const {Sequelize} = require('sequelize');
 const {ssl} = require("pg/lib/defaults");
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    // process.env.DB_NAME,
+    // process.env.DB_USER,
+    // process.env.DB_PASSWORD,
+    process.env.DB_LINK,
     {
         dialect: 'postgres',
         ssl: false,
