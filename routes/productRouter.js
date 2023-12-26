1903c946685cba2ec81b73c4d905649fab2767db
Osman <seitmemetov228@gmail.com>
2023-12-26 20:57:17 +0300

    changed /catalog endpoint

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,6 +4,7 @@ const productController = require('../controllers/productController');
 
 router.post('/', productController.create);
 router.get('/', productController.getAll);
+router.get('/main', productController.getMain);
 router.get('/:id', productController.getById);
 router.delete('/:id', productController.delete);
 router.put('/:id', productController.edit);
